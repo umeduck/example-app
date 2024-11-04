059bac123bb5bd3be975b019c5cc77f9b93a801a
umemura <ume.daihuku0116.0707@gmail.com>
2024-11-04 16:17:37 +0900

    chapter2-2

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -9,6 +9,18 @@
 <body>
   <h1>つぶやきアプリ</h1>
   <div>
+    <form action="{{ route('tweet.create') }}" method="post">
+      @csrf
+      <label for="tweet-content">つぶやき</label>
+      <span>140文字まで</span>
+      <textarea name="tweet" id="tweet-content" type="text" placeholder="つぶやきを入力"></textarea>
+      @error('tweet')
+      <p style="color: red;">{{ $message }}</p>
+      @enderror
+      <button type="submit">投稿</button>
+    </form>
+  </div>
+  <div>
     @foreach($tweets as $tweet)
       <p>{{ $tweet->content }}</p>
       <br>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\Sample\IndexController as Sample;
-use \App\Http\Controllers\Tweet\IndexController as Tweet;
+use \App\Http\Controllers\Sample\IndexController as SampleIndex;
+use \App\Http\Controllers\Tweet\IndexController as TweetIndex;
+use \App\Http\Controllers\Tweet\CreateController as TweetCreate;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sample', [Sample::class, 'show']);
+Route::get('/sample', [SampleIndex::class, 'show']);
 
-Route::get('/sample/{id}', [Sample::class, 'showId']);
+Route::get('/sample/{id}', [SampleIndex::class, 'showId']);
 
-Route::get('/tweet', Tweet::class);
+Route::get('/tweet', TweetIndex::class)->name('tweet.index');
+
+Route::post('/tweet/create', TweetCreate::class)->name('tweet.create');
