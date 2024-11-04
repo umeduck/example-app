d37582c3b038570cdba2af544de7e2a15bdf0dae
umemura <ume.daihuku0116.0707@gmail.com>
2024-11-04 13:55:11 +0900

    chapter2-1

---
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Tweet;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Tweet;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $tweets = Tweet::all();
+        
+        return view('tweet.index')->with('tweets', $tweets);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\Sample\IndexController as Sample;
+use \App\Http\Controllers\Tweet\IndexController as Tweet;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sample', [Sample::class, 'show']);
+
+Route::get('/sample/{id}', [Sample::class, 'showId']);
+
+Route::get('/tweet', Tweet::class);
